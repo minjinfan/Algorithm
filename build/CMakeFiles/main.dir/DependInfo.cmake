efdb2a2e339e4539a2387d29379b8fe70601de11
minjinfan <minjinfan@gmail.com>
2020-12-14 15:08:36 +0800

    add file structure

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/VS_WorkSpace/Algorithm/main.cpp" "E:/VS_WorkSpace/Algorithm/build/CMakeFiles/main.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "E:/VS_WorkSpace/Algorithm/build/src/CMakeFiles/srclib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/srclib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../lib/libsrclib.a"
+  "../../lib/libsrclib.pdb"
+  "CMakeFiles/srclib.dir/test.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/srclib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
